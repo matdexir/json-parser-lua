b2033d07c6889bf1913d7e8140cffe4708c3a3a1
matdexir <matthieudesir21@gmail.com>
2025-02-01 15:50:23 +0800

    chore: improve testing utils

--- test-utils.lua
+++ test-utils.lua
@@ -3,15 +3,15 @@ local io = require "io"
 local P = {}
 utils = P
 
-function P:checkOS() 
+function P:checkOS()
 	return package.config.sub(1, 1) == "\\" and "win" or "unix"
 end
 
 function P:filterArray(tbl, func)
 	local result = {}
-	
-	for i, v in ipairs(tbl) do 
-		if func(v) then 
+
+	for i, v in ipairs(tbl) do
+		if func(v) then
 			table.insert(result, v)
 		end
 	end
@@ -20,14 +20,14 @@ function P:filterArray(tbl, func)
 end
 
 function P:dirScan(directory)
-	if P:checkOS() == "win" then 
+	if P:checkOS() == "win" then
 		error("Sorry friend. Only unix like is supported.")
 	end
 
 	local fileTable = {}
-	local pFile = io.popen('ls -a "'.. directory ..'"') 
+	local pFile = io.popen('ls -a "'.. directory ..'"')
 
-	for line in pFile:lines() do 
+	for line in pFile:lines() do
 		table.insert(fileTable, line)
 	end
 
@@ -35,5 +35,29 @@ function P:dirScan(directory)
 
 	return fileTable
 end
+
+function P:printTable(t)
+    if type(t) ~= "table" then
+        error("Provided input is not a table!")
+    end
+
+    local function printTableRecursive(t, indent)
+        indent = indent or ""
+
+        for key, value in pairs(t) do
+            local formattedKey = type(key) == "number" and "["..key.."]" or key
+            if type(value) == "table" then
+                print(indent..formattedKey..": {")
+                printTableRecursive(value, indent.."  ")
+                print(indent.."}")
+            else
+                print(indent..formattedKey..": "..tostring(value))
+            end
+        end
+    end
+
+    printTableRecursive(t, "")
+end
+
 
 return utils
